b2b4550ab69a61918cb6fa4aff19bd33fae779e7
Richard Muvirimi <tygalive@gmail.com>
2023-02-01 21:24:05 +0200

    Fix crush when no email application is available

--- app/src/main/java/com/tyganeutronics/myratecalculator/fragments/FragmentSettings.kt
+++ app/src/main/java/com/tyganeutronics/myratecalculator/fragments/FragmentSettings.kt
@@ -1,13 +1,20 @@
 package com.tyganeutronics.myratecalculator.fragments
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PackageInfoFlags
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.tyganeutronics.myratecalculator.BuildConfig
 import com.tyganeutronics.myratecalculator.R
 import com.tyganeutronics.myratecalculator.utils.BaseUtils
 import de.psdev.licensesdialog.LicensesDialog
+import java.net.URI
 
 
 class FragmentSettings : PreferenceFragmentCompat(), Preference.OnPreferenceClickListener {
@@ -28,6 +35,21 @@ class FragmentSettings : PreferenceFragmentCompat(), Preference.OnPreferenceClic
 
         findPreference<Preference>(getString(R.string.donate))?.isVisible =
             BaseUtils.isPlayBuild().not()
+
+        findPreference<Preference>(getString(R.string.author_email))?.apply {
+            isVisible = intent?.let { intent ->
+                requireContext().packageManager.run {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        queryIntentActivities(
+                            intent,
+                            PackageManager.ResolveInfoFlags.of(0L)
+                        )
+                    } else {
+                        queryIntentActivities(intent, 0)
+                    }
+                }.isNotEmpty()
+            } == true
+        }
     }
 
     override fun onPreferenceClick(preference: Preference): Boolean {
