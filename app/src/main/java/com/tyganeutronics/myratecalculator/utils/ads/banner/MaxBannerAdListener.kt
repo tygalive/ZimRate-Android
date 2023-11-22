a430bdfdb4c4142bba339f65e9caa7249c47af66
Richard Muvirimi <richard@tyganeutronics.com>
2023-11-22 09:45:50 +0200

    Update Dependencies

--- app/src/main/java/com/tyganeutronics/myratecalculator/utils/ads/banner/MaxBannerAdListener.kt
+++ app/src/main/java/com/tyganeutronics/myratecalculator/utils/ads/banner/MaxBannerAdListener.kt
@@ -14,43 +14,43 @@ object MaxBannerAdListener : MaxAdViewAdListener, MaxAdRevenueListener {
 
     lateinit var contextRef: WeakReference<Context>
 
-    override fun onAdLoaded(p0: MaxAd?) {
+    override fun onAdLoaded(p0: MaxAd) {
 
     }
 
-    override fun onAdDisplayed(p0: MaxAd?) {
+    override fun onAdDisplayed(p0: MaxAd) {
 
     }
 
-    override fun onAdHidden(p0: MaxAd?) {
+    override fun onAdHidden(p0: MaxAd) {
 
     }
 
-    override fun onAdClicked(p0: MaxAd?) {
+    override fun onAdClicked(p0: MaxAd) {
 
         contextRef.get()?.let {
             FirebaseAnalytics.getInstance(it).logEvent("reward_banner_click", null)
         }
     }
 
-    override fun onAdLoadFailed(p0: String?, p1: MaxError?) {
+    override fun onAdLoadFailed(p0: String, p1: MaxError) {
 
     }
 
-    override fun onAdDisplayFailed(p0: MaxAd?, p1: MaxError?) {
+    override fun onAdDisplayFailed(p0: MaxAd, p1: MaxError) {
 
     }
 
-    override fun onAdExpanded(p0: MaxAd?) {
+    override fun onAdExpanded(p0: MaxAd) {
 
     }
 
-    override fun onAdCollapsed(p0: MaxAd?) {
+    override fun onAdCollapsed(p0: MaxAd) {
 
     }
 
-    override fun onAdRevenuePaid(impressionData: MaxAd?) {
-        impressionData?.let {
+    override fun onAdRevenuePaid(impressionData: MaxAd) {
+        impressionData.let {
 
             contextRef.get()?.let { context ->
 
