8dca4045f5478f4fcdbc94d15bb7e8fa0b7ffe68
Richard Muvirimi <richard@tyganeutronics.com>
2024-03-04 21:17:18 +0200

    Check for app updates

--- app/src/main/java/com/tyganeutronics/myratecalculator/ui/base/BaseAppActivity.kt
+++ app/src/main/java/com/tyganeutronics/myratecalculator/ui/base/BaseAppActivity.kt
@@ -1,8 +1,20 @@
 package com.tyganeutronics.myratecalculator.ui.base
 
+import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.google.android.play.core.review.ReviewInfo
 import com.google.android.play.core.review.ReviewManager
 import com.google.android.play.core.review.ReviewManagerFactory
+import com.tyganeutronics.myratecalculator.R
 import com.tyganeutronics.myratecalculator.interfaces.ReviewableActivity
 import com.tyganeutronics.myratecalculator.utils.BaseUtils
 import com.tyganeutronics.myratecalculator.utils.traits.getLongPref
@@ -15,6 +27,21 @@ abstract class BaseAppActivity : BaseAdActivity(), ReviewableActivity {
 
     private lateinit var reviewManager: ReviewManager
     private lateinit var reviewInfo: ReviewInfo
+
+    private lateinit var appUpdateManager: AppUpdateManager
+
+    private val listener = InstallStateUpdatedListener { state ->
+        if (state.installStatus() == InstallStatus.DOWNLOADED) {
+            Snackbar.make(
+                findViewById(R.id.layout_container),
+                getString(R.string.app_update_downloaded),
+                Snackbar.LENGTH_INDEFINITE
+            ).apply {
+                setAction(R.string.app_update_update) { appUpdateManager.completeUpdate() }
+            }.show()
+        }
+
+    }
 
     override fun syncViews() {
         if (BaseUtils.isPlayBuild || BaseUtils.isOtherBuild) {
@@ -49,6 +76,57 @@ abstract class BaseAppActivity : BaseAdActivity(), ReviewableActivity {
                 }
             }
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (BaseUtils.isPlayBuild || BaseUtils.isOtherBuild) {
+            checkAppHasUpdate()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (this::appUpdateManager.isInitialized) {
+            appUpdateManager.registerListener(listener)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        if (this::appUpdateManager.isInitialized) {
+            appUpdateManager.unregisterListener(listener)
+        }
+    }
+
+    private fun checkAppHasUpdate() {
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+
+        val activityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartIntentSenderForResult()
+        ) { _: ActivityResult ->
+            // handle callback
+        }
+
+        appUpdateManager
+            .appUpdateInfo
+            .addOnSuccessListener { appUpdateInfo ->
+                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                    && (appUpdateInfo.clientVersionStalenessDays() ?: -1) >= 7
+                    && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)
+                ) {
+                    // Request the update.
+                    appUpdateManager.startUpdateFlowForResult(
+                        appUpdateInfo,
+                        activityResultLauncher,
+                        AppUpdateOptions.newBuilder(AppUpdateType.FLEXIBLE).build()
+                    )
+
+                }
+            }
     }
 
 }
